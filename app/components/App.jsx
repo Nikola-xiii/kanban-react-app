a624716c2fe9df4a434ac1c781c059f7b761c550
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-14 13:26:20 +0200

    Connect NoteAction to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
 
 
 class App extends React.Component {
@@ -71,4 +72,6 @@ class App extends React.Component {
 
 export default connect(({notes}) => ({
   notes
-}))(App)
+}),{
+  NoteActions
+})(App)
