021fbeff9268b351a992f933aa1260600a55ad15
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-15 19:18:28 +0200

    Porting all action to App

--- app/stores/NodeStore.js
+++ app/stores/NodeStore.js
@@ -16,6 +16,7 @@ export default class NoteStore {
       }
     ]
   }
+
   create(note) {
     console.log('create note', note);
     this.setState({
@@ -25,8 +26,23 @@ export default class NoteStore {
 
   update(updatedNote) {
     console.log('update note', updatedNote);
+
+    this.setState({
+      notes: this.notes.map(note => {
+        if(note.id === updatedNote.id) {
+          return Object.assign({}, note, updatedNote);
+        }
+
+        return note;
+      })
+    });
   }
+
   delete(id) {
     console.log('delete note', id);
+
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
   }
 }
