b56d1475dfc24c4b21d41ec6e80fbb9ffc8651a0
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-25 14:36:44 +0200

    Add store to Alt

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,9 +1,11 @@
 import storage from '../../libs/storage';
 import persist from '../../libs/persist';
+import LaneStore from '../../stores/LaneStore';
 import NoteStore from '../../stores/NodeStore';
 
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+  alt.addStore('LAneStore', LaneStore);
 
   persist(alt, storage(localStorage), 'app');
 }
