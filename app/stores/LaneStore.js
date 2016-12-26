d993e152e7177bbf9ac2b0aa1ef545efd0e14d84
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-26 22:54:52 +0200

    Added 'attachToLane', 'detachFromLane' action to Lane Components

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -14,4 +14,28 @@ export default class LaneStore {
       lanes: this.lanes.concat(lane)
     });
   }
+
+  attachToLane({laneId, noteId}) {
+    this.setState({
+      lanes: this.lanes.map(lane => {
+        if(lane.notes.includes(noteId)) {
+          lane.notes = lane.notes.filter(note => note != noteId)
+        }
+
+        if(lane.id === laneId) {
+          lane.notes = lane.notes.concat([noteId]);
+        }
+      })
+    });
+  }
+
+  detachFromLane({laneId, noteId}) {
+    lanes: this.lanes.map(lane => {
+      if(lane.id === laneId) {
+        lane.notes = lane.notes.filter(note => note !== noteId)
+      }
+
+      return lane;
+    });
+  }
 }
