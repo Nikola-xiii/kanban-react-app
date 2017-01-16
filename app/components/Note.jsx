1bc444d4ac1c891e8954a27977ca2a0abce5d61b
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-16 16:28:45 +0200

    Added isOver and isDragging

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -4,11 +4,11 @@ import {DragSource, DropTarget} from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const Note = ({
-  connectDragSource, connectDropTarget,
-  onMove, id, children, ...props
+  connectDragSource, connectDropTarget, isDragging,
+  isOver, onMove, id, children, ...props
 }) => {
   return compose(connectDragSource, connectDropTarget)(
-    <div {...props}>
+    <div style={{opacity: isDragging || isOver ? 0 : 1}} {...props}>
       {children}
     </div>
   )
@@ -38,11 +38,13 @@ const noteTarget = {
 
 export default compose(
   DragSource(ItemTypes.NOTE, noteSource,
-    connect => ({
-      connectDragSource: connect.dragSource()
-    })),
+    (connect, monitor) => ({
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+      })),
     DropTarget(ItemTypes.NOTE, noteTarget,
-      connect => ({
-      connectDropTarget: connect.dropTarget()
-    }))
+      (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
+      }))
   )(Note)
