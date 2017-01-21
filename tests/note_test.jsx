4d2cd1decd83d6b5559da0fd1f8247138f8a03dc
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-21 13:35:45 +0200

    Write test for Note Component render

---
+++ tests/note_test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {renderIntoDocument} from 'react-addons-test-utils';
+import TestBacked from 'react-dnd-test-backend';
+import {DragDropContext} from 'react-dnd';
+import assert from 'assert';
+import Note from '../app/components/Note';
+
+describe('Note', function () {
+  it('renders children', function () {
+    const test = 'test';
+    const NoteContent = wrapInTestContext(Note);
+    const component = renderIntoDocument(
+      <NoteContent id="demo">{test}</NoteContent>
+    );
+
+    assert.equal(component.props.children, test);
+  });
+});
+
+function wrapInTestContext(DecoratedComponent) {
+  @DragDropContext(TestBacked)
+  class TestContextContainer extends React.Component {
+    render() {
+      return <DecoratedComponent {...this.props}/>
+    }
+  }
+
+  return TestContextContainer;
+}
